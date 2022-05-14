f3edfc76a743f1075125fc6815b53af41344395d
ti-bone <59691680+ti-bone@users.noreply.github.com>
2022-05-14 10:49:59 -0300

    Члены? Сосал.

--- src/index.js
+++ src/index.js
@@ -2,18 +2,25 @@ const { Telegraf } = require("telegraf");
 const bot = new Telegraf("your bot token");
 const os = require("os");
 
-bot.on("message", (ctx) => {
+bot.on("message", async (ctx) => {
     if(ctx.chat.type == "group") return;
     if(ctx.from.id == "136817688") {
         try{
-            ctx.deleteMessage();
+            await ctx.deleteMessage().catch((err) => {
+                console.error(err)
+            });
+
+            await bot.telegram.banChatSenderChat(ctx.chat.id, ctx.message.sender_chat.id).catch((err) => {
+                console.error(err)
+            });
             return;
         }
         catch(error){
             console.error(error);
         }
-    };
+    }
 });
 
-bot.launch();
-console.log("AnonProtect Bot started sucessfully on: \n" + os.type() + " (" + os.platform() + ") " + os.release());
+bot.launch().then(() => {
+    console.log("AnonProtect Bot started sucessfully on: \n" + os.type() + " (" + os.platform() + ") " + os.release());
+});
